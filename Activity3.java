38e08d7eb0678685f2f20f536d051b0a5193aa49
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-02-03 11:12:45 -0700

    Activity 3
    object array lists

--- Activity3.java
+++ Activity3.java
@@ -28,13 +28,14 @@ public class Activity3 {
 	}
 	
 	public String toString() {
-		return "Brand: " + brand + ", Resolution: " + resW + "x" + resH + " |";
+		return "(Brand: " + brand + ", Resolution: " + resW + "x" + resH + ")";
 	}
 	
 	public static void main(String[] args) {
 		ArrayList<Activity3> tv = new ArrayList<Activity3>();
 		
 		boolean reset=true;
+		boolean hasRunOnce=false;
 		
 		do {
 			System.out.println("Select an option: \n1. Add a new tv\n2. exit");
@@ -43,6 +44,15 @@ public class Activity3 {
 			if(option == 1) {
 				Activity3 tv0 = new Activity3();
 				tv.add(tv0);
+				
+				if(hasRunOnce == true) {
+					System.out.println("Arranging TV to top...");
+					tv.add(tv.get(tv.size())); // temporarily duplicate most recently added element
+					tv.add(tv.get(1)); // duplicate first slot to move it to the end
+					tv.set(1, tv.get(tv.size()-1)); // set slot 1 to be the last element - 1
+					tv.remove(tv.size()-1); // remove temporary element
+				}
+				hasRunOnce = true;
 			}
 			else {
 				break;
