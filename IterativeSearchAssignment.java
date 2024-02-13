25f63375696b0851e06ccb4e95aaef61eb70e61e
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-02-13 13:58:01 -0700

    added binary search to iterative search assignment

--- IterativeSearchAssignment.java
+++ IterativeSearchAssignment.java
@@ -5,33 +5,87 @@ import java.util.Scanner;
 public class IterativeSearchAssignment {
 	static Scanner input = new Scanner(System.in);
 	
-	static void iterativeSearch(int[] a) {
-		System.out.println("Input a number to search: ");
+	static void linearSearch(int[] a) {
+		System.out.println("(Linear) Input a number to search: ");
 		int numIn = input.nextInt();
+		
 		int i=0;
 		boolean indexIdentified = false;
 		int numAmt=0;
 		
-		for(i = 0; i < a.length; ++i) {
+		for(i = 0; i < a.length; ++i) { // runs through and identifies matching numbers and prints each one with its respective index 
 			if(numIn == a[i]) {
 				System.out.printf("The number is at index %d or position %d\n", i, i+1);
 				indexIdentified = true;
-				++numAmt;
+				++numAmt; // number of instances 
 			}
 		}
 		
-		if(i == a.length && indexIdentified == false) {
-			System.out.println("That number is not in the array");
+		if(i == a.length && indexIdentified == false) { // if it reaches the end of the array and no numbers have been identified
+			System.out.println("That number is not in the array"); 
 		}
-		if(indexIdentified == true) {
+		if(indexIdentified == true) { // if numbers have been identified, print the amount
 			System.out.printf("There are %d instance(s) of this number in the array", numAmt);
+		}
+	}
+	
+	static void binarySearch(int[] a) {
+		/// sorting
+		int itemToInsert, j;
+		boolean reset;
+		
+		for(int k = 1; k < a.length; k++) {
+			itemToInsert = a[k]; 
+			j = k-1; 
+			reset = false;
+			
+			while((j >= 0) && !reset) {
+				if (itemToInsert < a[j] ) {
+					a[j + 1] = a[j];
+					j--;
+					if(j == -1)
+					a[0] = itemToInsert;
+				}
+				else {
+					reset = true;
+					a[j + 1] = itemToInsert;
+				}
+			}
+		}
+		
+		/// binary search with sorted array
+		
+		System.out.println("\n\n(Binary) Input a number to search: ");
+		int numIn = input.nextInt();
+		
+		int low = 0;
+		int high = a.length;
+		
+		while(low+1 < high) {
+			int temp = (low + high)/2; // move index to middle of array
+			
+			if(a[temp] > numIn) { // if the currently selected element is greater than the input number then set the index to the high variable, otherwise set it to the low variable
+				high = temp;
+			}
+			else {
+				low = temp;
+			}
+		}
+		
+		if(a[low] == numIn) { // if a exact match is found, print its index
+			System.out.printf("The first instance of the number is at index %d or position %d\n", low, low+1);
+		}
+		else {
+			System.out.println("\nThat number is not in the array");
 		}
 	}
 	
 	
 	public static void main(String[] args) {
 		int[] OriginalArray = {4,10,6,1,8,10,9,12,14,6,15,6,7,10,8,2,7,3,9,1};
+		int[] newOriginalArray = {4,10,6,1,8,10,9,12,14,6,15,6,7,10,8,2,7,3,9,1};
 		
-		iterativeSearch(OriginalArray);
+		linearSearch(OriginalArray);
+		binarySearch(newOriginalArray);
 	}
 }
