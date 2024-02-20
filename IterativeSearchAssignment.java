f03bb5900e54911cfa8e9fc447c9ead8d08553ab
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-02-20 13:13:19 -0700

    corrected binary search section

--- IterativeSearchAssignment.java
+++ IterativeSearchAssignment.java
@@ -60,24 +60,61 @@ public class IterativeSearchAssignment {
 		
 		int low = 0;
 		int high = a.length;
+		int m = 0;
+		int n = 0;
 		
-		while(low+1 < high) {
-			int temp = (low + high)/2; // move index to middle of array
+		do {
+			m = 0;
+			int temp=0;
+			while(low+1 < high) {
+				
+				temp = (low + high)/2;// move index to middle of array
+				
+				if(a[temp] > numIn) { // if the currently selected element is greater than the input number then set the index to the high variable, otherwise set it to the low variable
+					high = temp;
+				}
+				else {
+					low = temp;
+				}
+			}
 			
-			if(a[temp] > numIn) { // if the currently selected element is greater than the input number then set the index to the high variable, otherwise set it to the low variable
-				high = temp;
+			if(a[low] == numIn) { // if a exact match is found, print its index
+				System.out.printf("The number is at index %d or position %d\n", low, low+1);
+				
+				//System.out.printf("The first instance of the number is at index %d or position %d\n", low, low+1);
+				m++;
+				n++;
+				if(numIn != 0) { // need some way to add in an element to replace and eliminate a found element when identifying multiple instances of the element
+					a[low] = 0;
+				}
+				else {
+					a[low] = 1; 
+				}
+				for(int k = 1; k < a.length; k++) { // re-sort
+					itemToInsert = a[k]; 
+					j = k-1; 
+					reset = false;
+					
+					while((j >= 0) && !reset) {
+						if (itemToInsert < a[j] ) {
+							a[j + 1] = a[j];
+							j--;
+							if(j == -1)
+							a[0] = itemToInsert;
+						}
+						else {
+							reset = true;
+							a[j + 1] = itemToInsert;
+						}
+					}
+				}
 			}
 			else {
-				low = temp;
+				System.out.println("\nThat number is not in the array");
 			}
-		}
+		}while(m == 1);
 		
-		if(a[low] == numIn) { // if a exact match is found, print its index
-			System.out.printf("The first instance of the number is at index %d or position %d\n", low, low+1);
-		}
-		else {
-			System.out.println("\nThat number is not in the array");
-		}
+		System.out.printf("There is %d instances of this number in the array", n);
 	}
 	
 	
