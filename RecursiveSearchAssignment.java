f0659ed75768e5652f09fce65d01ba7894ef2486
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-03-04 15:38:51 -0700

    Updated recursive search with correct information

--- RecursiveSearchAssignment.java
+++ RecursiveSearchAssignment.java
@@ -5,67 +5,88 @@ import java.util.Scanner;
 public class RecursiveSearchAssignment {
 	static Scanner input = new Scanner(System.in);
 	
-	static int[] insertionSort(int[] a) { // throw the array into a sorter and return it sorted
-			int itemToInsert, j;
-			boolean reset;
-				
-			for(int k = 1; k < a.length; k++) {
-				itemToInsert = a[k]; 
-				j = k-1; 
-				reset = false;
-					
-				while((j >= 0) && !reset) {
-					if (itemToInsert < a[j] ) {
-						a[j + 1] = a[j];
-						j--;
-						if(j == -1)
+	static int[] insertionSort(int[] a) { // array sorter
+		int itemToInsert, j;
+		boolean reset;
+
+		for(int k = 1; k < a.length; k++) {
+			itemToInsert = a[k]; 
+			j = k-1; 
+			reset = false;
+
+			while((j >= 0) && !reset) {
+				if (itemToInsert < a[j] ) {
+					a[j + 1] = a[j];
+					j--;
+					if(j == -1)
 						a[0] = itemToInsert;
-					}
-					else {
-						reset = true;
-						a[j + 1] = itemToInsert;
-					}
+				}
+				else {
+					reset = true;
+					a[j + 1] = itemToInsert;
 				}
 			}
-			// XXX debug
-			System.out.print("Sorted array: ");
-			for(int l = 0; l < a.length; ++l) {
-				System.out.print(a[l]+ " ");
-			}
-			return a;
+		}
+
+		// XXX debug
+		System.out.println("\n");
+		for(int n = 0; n < a.length; ++n) {
+			System.out.print(a[n]+ " ");
+		}
+
+		return a;
 	}
 	
-	public static int recursiveBinary(int[] a, int low, int high, int numIn) {						
-		if(low == numIn) {
-			return index;
+	public static int recursiveBinary(int[] a, int low, int high, int numIn) {
+		int mid = 0;
+		int count = 0;
+		
+		while(low <= high) {
+			
+			//Find mid point
+			mid = (low + high)/2; 
+			
+			//is value at mid equal to target
+			//return mid
+			//index++;
+			if(a[mid] == numIn) { 
+				++count; // keeps track of number instances
+
+				for(int k = mid+1; k <= a.length-1; ++k) { 
+					if(a[k] == numIn)
+						count++;
+				}
+				for(int k = mid-1; k > -1; --k) { 
+					if(a[k] == numIn)
+						count++;
+				}
+				return count;
+			}
+			
+			//is it greater?
+			//recursively call function but with low and mid-1 for high
+			else if(a[mid] > numIn) {
+				return recursiveBinary(a, low, mid-1, numIn);
+			}
+			
+			//is it less
+			//recursively call function but with mid+1 for low and high
+			else {
+				return recursiveBinary(a, mid+1, high, numIn);
+			}
 		}
-		
-		//Find mid point
-		
-		
-		//is value at mid equal to target
-		//return mid
-		
-		//is it greater?
-		//recursively call function but with low and mid-1 for high
-		
-		//is it less
-		//recursively call function but with mid+1 for low and high
-		
-		++index;
-		return recursiveBinary(a, low+1, high, numIn, index);
+		return 0;	
 	}
 	
 	public static void main(String[] args) {
 		int[] OriginalArray = {4,10,6,1,8,10,9,12,14,6,15,6,7,10,8,2,7,3,9,1};
-		int index = 0;
 		
-		System.out.println("Input number to search: ");
+		System.out.println("Input number to search: "); // input target number
 		int numIn = input.nextInt();
 		
-		int[] newArray = insertionSort(OriginalArray);
-		int output = recursiveBinary(newArray, 0, newArray.length-1, numIn, index); // new sorted array put into recursive search 
-		
-		System.out.println("\n\n" + output);
+		int[] sortedArray = insertionSort(OriginalArray);
+		int count = recursiveBinary(sortedArray, 0, sortedArray.length-1, numIn); // new sorted array put into recursive search 
+										// a		       low									high			  target
+		System.out.println("\n\nThere are " + count + " instances of this number in the array");
 	}
 }
