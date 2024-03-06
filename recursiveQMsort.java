d9901ede7c12241f92a81dd441235ae713796254
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-03-06 14:55:08 -0700

    completed recursive quick and merge sort

--- recursiveQMsort.java
+++ recursiveQMsort.java
@@ -1,9 +1,9 @@
 package Activity;
 
 public class recursiveQMsort {
-	public static void mergeSort(int[] a) {
+	public static int[] mergeSort(int[] a) {
 		if (a.length <= 1) {
-            return;
+            return a;
         }
 		
         int mid = a.length / 2; // find mid point
@@ -19,7 +19,8 @@ public class recursiveQMsort {
         mergeSort(leftArray); // sort the left side
         mergeSort(rightArray); // sort the right side
         merge(a, leftArray, rightArray); // recur until length is less than or equal to 1
-
+        
+        return a;
 	}
 
 	public static void merge(int[] a, int[] leftArray, int[] rightArray) {
@@ -40,14 +41,65 @@ public class recursiveQMsort {
 		}
 
 	}
+	
+	///
+	
+	static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+ 
+    static int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low - 1);
+ 
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    static int[] quickSort(int[] arr, int low, int high)
+    {
+        if (low < high) {
+        	int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+        return arr;
+    }
+    
+    public static void printArr(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }	
 
 	public static void main(String[] args) {
-		int[] array = {5,3,7,8,2,1,9,4,10,6};
+		int[] array = {5,3,7,8,2,1,9,4,10,6}, out1, out2;
+		
+		out1 = mergeSort(array);
+		out2 = quickSort(array, 0, array.length-1);
 
-		mergeSort(array);
-
+		System.out.println("(merge)");
+		
 		for(int i = 0; i < array.length; ++i) {
-			System.out.println(array[i]);
+			System.out.println(out1[i]);
+		}
+		
+		System.out.println("\n(quick)");
+		
+		for(int i = 0; i < array.length; ++i) {
+			System.out.println(out2[i]);
 		}
 	}
 }
