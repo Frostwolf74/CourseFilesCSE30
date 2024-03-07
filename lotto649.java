1047f53e198cc600c189f77766983f5762880a1f
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-03-06 21:04:56 -0700

    Created lotto 649 assignment
    added randomizer created by chatGPT

---
+++ lotto649.java
@@ -0,0 +1,31 @@
+package Activity;
+
+import java.util.Random;
+
+public class lotto649 {
+	public static int[] randomizeArray(int amt) { // randomizes array without repeating digits 
+		int[] a = new int[amt];
+		for(int i = 0; i < amt; ++i){
+			a[i] = i+1;
+		}
+
+		Random rand = new Random();
+
+        for (int i = a.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+
+            int temp = a[i];
+            a[i] = a[index];
+            a[index] = temp;
+        }
+        return a;
+    }
+	
+	public static void main(String[] args) {		
+        int[] out = randomizeArray(49);
+        
+        for(int i=0; i < out.length; i++) {
+        	System.out.println(out[i]);
+        }
+	}
+}
