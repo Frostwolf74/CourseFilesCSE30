d443e7b5ad64843df7dee6853163f7297cfc038f
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-03-07 13:55:08 -0700

    updated lotto649 with proof of concept code

--- lotto649.java
+++ lotto649.java
@@ -27,5 +27,18 @@ public class lotto649 {
         for(int i=0; i < out.length; i++) {
         	System.out.println(out[i]);
         }
+        
+        System.out.println("Winning numbers: ");
+        
+        int[] winningNumbers = new int[6];
+        for(int i=0; i < winningNumbers.length; ++i) { // picking 6
+        	winningNumbers[i] = out[i];        
+        }
+        
+        // XXX debug print
+        for(int i=0; i < winningNumbers.length; ++i) {
+        	System.out.println(winningNumbers[i]);
+        }
 	}
+	
 }
