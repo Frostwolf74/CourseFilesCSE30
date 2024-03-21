9537b8140589927b5e5af1612fe0b8de76965542
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-03-21 13:48:18 -0600

    completed

--- Inheritance.java
+++ Inheritance.java
@@ -1,17 +1,6 @@
 package Activity;
 
-//Create an Object class for a motorized vehicle.
-//
-//This class should consist of various object variables:
-//
-//Make
-//Model
-//Colour
-//Mileage
-//Year
-//Create 3 or more methods, to display various elements of date (ex: display mileage) or create a method that will allow the user to enter the variable data themselves.
-//
-//Within your main, create 2 individual objects with various data, or create two empty objects and use a method to manually enter the data.
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Inheritance {
@@ -83,12 +72,14 @@ public class Inheritance {
 		this.year = year;
 	}
 	
+	
 	public String toString() {
 		return "make: " + make + "\nmodel: " + model + "\ncolour: " + colour + "\nmilage: " + milage + "\nyear: " + year;
 	}
 
 	public void setAll() {
 		System.out.println("Enter make: ");
+		input.nextLine();
 		this.make = input.nextLine();
 		System.out.println("Enter model: ");
 		this.model = input.nextLine();
@@ -100,20 +91,61 @@ public class Inheritance {
 		System.out.println("Enter year: ");
 		this.year = input.nextInt();
 		input.nextLine();
+		System.out.println(toString());
 	}
 	
-	public void method2() {
-		System.out.println("This is method2");
+	public void showMilage() {
+		System.out.println(this.milage);
 	}
 	
-	public void method3() {
-		System.out.println("This is method3");
+	public void showYear() {
+		System.out.println(this.year);
 	}
 
 	public static void main(String[] args) {
 		Inheritance car = new Inheritance();
+		System.out.println(car);
 		
+		int option=0; 
+		boolean reset=false;
 		
+		do {
+			reset=false;
+			System.out.println("Choose an option: 1. re-input values, 2. show milage, 3. show year, 4. exit");
+			
+			if(input.hasNextInt()) { // check for integer
+				option = input.nextInt();
+			}
+			else { // if input is not an integer
+				System.out.println("invalid input");
+				reset=true;
+			}
+
+			switch(option) {
+			case 1:
+				car.setAll();
+				break;
+			case 2:
+				System.out.println(car.getMilage());
+				break;
+			case 3:
+				System.out.println(car.getYear());
+				break;
+			case 4: 
+				System.exit(0);
+				break;
+			default:
+				System.out.println("invalid input");
+				reset=true;
+				break;
+			}
+		}while(reset);
+		
+		newArrayList();
+	}
+	
+	public static void newArrayList() {
+		ArrayList<Inheritance> carList = new ArrayList<Inheritance>();
 	}
 }
 
