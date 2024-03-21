30cf746c7f39201daccb93dcfc89cbbd48c6ec35
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-03-21 17:57:36 -0600

    added motorcycle inheritance activity

---
+++ MotorCycleInheritance.java
@@ -0,0 +1,130 @@
+package inheritance;
+
+import java.util.Scanner;
+
+public class MotorCycleInheritance {
+	private String make, model, colour;
+	private int milage, year;
+	
+	static Scanner input = new Scanner(System.in);
+	
+	public MotorCycleInheritance(String make, String model, String colour, int milage, int year) {
+		this.make = make;
+		this.model = model;
+		this.colour = colour;
+		this.milage = milage;
+		this.year = year;
+	}
+	
+	public MotorCycleInheritance() {
+		System.out.println("Input make: ");
+		setMake(input.nextLine());
+		System.out.println("Input model: ");
+		setModel(input.nextLine());
+		System.out.println("Input colour: ");
+		setColour(input.nextLine());
+		System.out.println("Input milage: ");
+		setMilage(input.nextInt());
+		System.out.println("Input year: ");
+		setYear(input.nextInt());
+	}
+	
+	public void displayMilage() {
+		System.out.println(getMilage());
+	}
+	
+	public void displayAllString() {
+		System.out.printf("Make: %s\nModel: %s\nColour: %s", this.make, this.model, this.colour);
+	}
+	
+	public void setYear() {
+		System.out.println("Input new year: ");
+		int newYear = input.nextInt();
+		
+		setYear(newYear);
+	}
+	
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public int getMilage() {
+		return milage;
+	}
+
+	public void setMilage(int milage) {
+		this.milage = milage;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public String toString() {
+		return "Make: " + make + "\nModel: " + model + "\nColour: " + colour + "\nMilage: " + milage + "\nYear: " + year;
+	}
+
+	public static void main(String[] args) {
+		MotorCycleInheritance motorCycle = new MotorCycleInheritance();
+		
+		System.out.println(motorCycle);
+		
+		int option=-1;
+		boolean reset=false;
+		do {reset=false;
+			System.out.println("Select an option: 1. Show milage, 2. Show all, 3. Edit year");
+			input.nextLine();
+			
+			if(input.hasNextInt()) {
+				option = input.nextInt();
+			}
+			else {
+				System.out.println("Invalid input");
+				reset = true;
+			}
+
+			switch(option) {
+				case 1:
+					motorCycle.displayMilage();
+					break;
+				case 2:
+					motorCycle.displayAllString();
+					break;
+				case 3:
+					motorCycle.setYear();
+					break;
+				case -1:
+					break;
+				default:
+					System.out.println("Invalid input");
+					reset=true;
+					break;
+			}
+		}while(reset);
+		System.out.println(motorCycle);
+	}
+}
